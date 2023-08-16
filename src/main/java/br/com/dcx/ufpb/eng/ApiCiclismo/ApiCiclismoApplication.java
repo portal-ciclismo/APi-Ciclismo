67fcc909810d8b31e278b16ab17964fa97f34204
Vinicius Dias <105192228+viniciusDias1001@users.noreply.github.com>
2023-08-15 21:13:03 -0300

    Base Project

---
+++ src/main/java/br/com/dcx/ufpb/eng/ApiCiclismo/ApiCiclismoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dcx.ufpb.eng.ApiCiclismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCiclismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCiclismoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dcx/ufpb/eng/ApiCiclismo/ApiCiclismoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dcx.ufpb.eng.ApiCiclismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCiclismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
