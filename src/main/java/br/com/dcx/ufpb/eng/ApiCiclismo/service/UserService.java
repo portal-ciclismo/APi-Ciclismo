904ce102e64b504b18bcccf3972740b4f1e1ce27
guilhecsr <guilhe.csr@outlook.com>
2023-08-18 23:52:19 -0300

    criaçãoServiceUser#11

---
+++ src/main/java/br/com/dcx/ufpb/eng/ApiCiclismo/service/UserService.java
@@ -0,0 +1,39 @@
+package br.com.dcx.ufpb.eng.ApiCiclismo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+     
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long userId, User user) {
+        // 
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long userId) {
+        // 
+        userRepository.deleteById(userId);
+    }
+
+    public User getUserById(Long userId) {
+        // 
+        return userRepository.findById(userId).orElse(null);
+    }
+
+    public List<User> getAllUsers() {
+        // 
+        return userRepository.findAll();
+    }
+}
