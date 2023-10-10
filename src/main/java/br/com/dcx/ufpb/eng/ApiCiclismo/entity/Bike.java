1d3354ceaa5f92d01d22bd301101e42f46c1503a
Gabriel Leandro Pontes da Silva <gabriel.pontes@dcx.ufpb.br>
2023-10-09 23:26:14 -0300

    criado entidade bike

---
+++ src/main/java/br/com/dcx/ufpb/eng/ApiCiclismo/entity/Bike.java
@@ -0,0 +1,66 @@
+package br.com.dcx.ufpb.eng.ApiCiclismo.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+import java.io.Serial;
+
+@Entity
+public class Bike {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Size(max = 30)
+    private String frameMaterial;
+
+    @Size(max = 30)
+    private String frameBrand;
+
+    @Size(max = 2)
+    private Integer wheelSize;
+
+    public Bike(String frameMaterial, String frameBrand, Integer wheelSize) {
+        this.frameMaterial = frameMaterial;
+        this.frameBrand = frameBrand;
+        this.wheelSize = wheelSize;
+    }
+
+    public Bike() {
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getFrameMaterial() {
+        return frameMaterial;
+    }
+
+    public void setFrameMaterial(String frameMaterial) {
+        this.frameMaterial = frameMaterial;
+    }
+
+    public String getFrameBrand() {
+        return frameBrand;
+    }
+
+    public void setFrameBrand(String frameBrand) {
+        this.frameBrand = frameBrand;
+    }
+
+    public Integer getWheelSize() {
+        return wheelSize;
+    }
+
+    public void setWheelSize(Integer wheelSize) {
+        this.wheelSize = wheelSize;
+    }
+}
