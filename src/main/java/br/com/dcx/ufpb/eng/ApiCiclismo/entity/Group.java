563f17e8280c052ac7c31b52dddc90c27f56266b
Ricardo-Viana <ricardo.macedo@dcx.ufpb.br>
2023-10-15 14:41:09 -0300

    Implementado Grupo Controller

--- src/main/java/br/com/dcx/ufpb/eng/ApiCiclismo/entity/Group.java
+++ src/main/java/br/com/dcx/ufpb/eng/ApiCiclismo/entity/Group.java
@@ -1,6 +1,7 @@
 package br.com.dcx.ufpb.eng.ApiCiclismo.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.io.Serial;
 
@@ -28,7 +29,7 @@ public class Group {
     private String location;
 
     @Column(name = "PRIVACY")
-    @NotBlank(message = "Privacy cannot be empty")
+    @NotNull(message = "Privacy cannot be empty")
     private boolean privacy;
 /*
 
