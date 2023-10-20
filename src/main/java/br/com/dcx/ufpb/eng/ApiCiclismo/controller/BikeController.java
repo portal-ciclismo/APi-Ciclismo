253b030e43e19a15fe4b9c249901b48ac0bf8d50
Gabriel Leandro Pontes da Silva <gabriel.pontes@dcx.ufpb.br>
2023-10-20 20:23:34 -0300

    Inserido o crossOrigin no controller bike

--- src/main/java/br/com/dcx/ufpb/eng/ApiCiclismo/controller/BikeController.java
+++ src/main/java/br/com/dcx/ufpb/eng/ApiCiclismo/controller/BikeController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/bike")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BikeController {
 
     private BikeService bikeService;
